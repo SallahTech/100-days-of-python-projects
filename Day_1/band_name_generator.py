b36d7d885e6292965d2a21612f7f98339ee55393
sallahtech <sallahtech@localhost.localdomain>
2022-05-06 23:03:20 +0300

    Adding day one project- which is a simple random band name generator

---
+++ Day_1/band_name_generator.py
@@ -0,0 +1,12 @@
+# 1. Create a greeting for your program
+print('Welcome to the Band Name Generator.')
+
+# 2. Ask the user for the city that they grow up in.
+city = input("What's the name of the city you grow up in?\n")
+
+# 3. Ask the user for the name of a pet
+pet = input("What's your pet name?\n")
+
+# 4. Combine the name of their city and pet name to show their name.
+band_name = city + " " + pet
+print("Your band name could be " + band_name)
